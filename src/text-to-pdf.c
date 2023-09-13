eb25236d1976e2a5b92f3935a112ff53fe960a9d
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2023-09-13 19:55:17 +0200

    code : working on understanding why redirecting stdout to a file doesn't work as intended

--- src/text-to-pdf.c
+++ src/text-to-pdf.c
@@ -37,18 +37,18 @@ int slPos[NPAGE];
 int xrefPos;
 
 
-int escape(char *s,FILE* out){
+int escape(char *s){
 	int n = 0;
 	while (*s) {
 		switch (*s) {
             case '(':
             case ')':
             case '\\':{
-                fprintf(out,"%d",'\\'); //since (,) and \\ are special char, we need to escape them
+                putchar('\\'); //since (,) and \\ are special char, we need to escape them
                 n++;
             }
 		    default:{
-                fprintf(out,"%s",s);
+                putchar(*s);
                 n++;
             }
 		}
@@ -57,27 +57,27 @@ int escape(char *s,FILE* out){
 	return n;
 }
 
-int genCs(FILE *in,FILE* out){
+int genCs(FILE *in){
 	char line[NCOL + 1];
 	int n, nl, eol, ch;
 	n = 0;
-	n += fprintf(out,"BT\n");
-	n += fprintf(out,"11 TL\n");
-	n += fprintf(out,"34 784 Td\n");
-	n += fprintf(out,"/F0 11 Tf\n");
+	n += printf("BT\n");
+	n += printf("11 TL\n");
+	n += printf("34 784 Td\n");
+	n += printf("/F0 11 Tf\n");
 	nl = 0;
 	while (fgets(line, sizeof(line), in)) {
 		eol = strcspn(line, "\n");
 		if (!line[eol] && (ch = fgetc(in)) != '\n')
 			ungetc(ch, in);
 		line[eol] = '\0'; //because null terminated strings
-		n += fprintf(out,"T* (");
-		n += escape(line,out);
-		n += fprintf(out,") Tj\n");
+		n += printf("T* (");
+		n += escape(line);
+		n += printf(") Tj\n");
 		if (++nl == NROW)
 			break;
 	}
-	n += fprintf(out,"ET");
+	n += printf("ET");
 	nByte += n;
 	return n;
 }
@@ -86,142 +86,139 @@ void genHead(void) {
 	nByte += printf("%%PDF-1.4\n");
 }
 
-void genCl(FILE* out){
+void genCl(void){
 	clPos = nByte;
-	nByte += fprintf(out,"1 0 obj\n");
-	nByte += fprintf(out,"<< /Type /Catalog /Pages 2 0 R >>\n");
-	nByte += fprintf(out,"endobj\n");
+	nByte += printf("1 0 obj\n");
+	nByte += printf("<< /Type /Catalog /Pages 2 0 R >>\n");
+	nByte += printf("endobj\n");
 }
 
-int genPc(FILE* in,FILE* out){
+int genPc(FILE* in){
 	int sl;
 	pcPos[nPage] = nByte;
-	nByte += fprintf(out,"%d 0 obj\n", PC_BASE + nPage);
-	nByte += fprintf(out,"<< /Length %d 0 R >>\n", SL_BASE + nPage);
-	nByte += fprintf(out,"stream\n");
-	sl = genCs(in,out);
-	nByte += fprintf(out,"\nendstream\n");
-	nByte += fprintf(out,"endobj\n");
+	nByte += printf("%d 0 obj\n", PC_BASE + nPage);
+	nByte += printf("<< /Length %d 0 R >>\n", SL_BASE + nPage);
+	nByte += printf("stream\n");
+	sl = genCs(in);
+	nByte += printf("\nendstream\n");
+	nByte += printf("endobj\n");
 	return sl;
 }
 
 
-void genSl(int n,FILE* out){
+void genSl(int n){
 	slPos[nPage] = nByte;
-	nByte += fprintf(out,"%d 0 obj\n", SL_BASE + nPage);
-	nByte += fprintf(out,"%d\n", n);
-	nByte += fprintf(out,"endobj\n");
+	nByte += printf("%d 0 obj\n", SL_BASE + nPage);
+	nByte += printf("%d\n", n);
+	nByte += printf("endobj\n");
 }
 
-void genPg(FILE* out) {
+void genPg(void) {
 	pgPos[nPage] = nByte;
-	nByte += fprintf(out,"%d 0 obj\n", PG_BASE + nPage);
-	nByte += fprintf(out,"<< /Type /Page\n");
-	nByte += fprintf(out,"/Parent 2 0 R\n");
-	nByte += fprintf(out,"/Contents %d 0 R >>\n", PC_BASE + nPage);
-	nByte += fprintf(out,"endobj\n");
+	nByte += printf("%d 0 obj\n", PG_BASE + nPage);
+	nByte += printf("<< /Type /Page\n");
+	nByte += printf("/Parent 2 0 R\n");
+	nByte += printf("/Contents %d 0 R >>\n", PC_BASE + nPage);
+	nByte += printf("endobj\n");
 }
 
-void genPr(FILE* out){
+void genPr(void){
 	prPos = nByte;
-	nByte += fprintf(out,"2 0 obj\n");
-	nByte += fprintf(out,"<< /Type /Pages\n");
-	nByte += fprintf(out,"/Kids [\n");
+	nByte += printf("2 0 obj\n");
+	nByte += printf("<< /Type /Pages\n");
+	nByte += printf("/Kids [\n");
 	for (int i = 0; i < nPage; i++)
-		nByte += fprintf(out,"%d 0 R\n", PG_BASE + i);
-	nByte += fprintf(out,"]\n");
-	nByte += fprintf(out,"/Count %d\n", nPage);
-	nByte += fprintf(out,"/MediaBox [0 0 595 842]\n");
-	nByte += fprintf(out,"/Resources << /Font << /F0 <<\n");
-	nByte += fprintf(out,"/Type /Font\n");
-	nByte += fprintf(out,"/Subtype /Type1\n");
-	nByte += fprintf(out,"/BaseFont /Courier\n");
-	nByte += fprintf(out,">> >> >>\n");	/* end of resources dict */
-	nByte += fprintf(out,">>\n");
-	nByte += fprintf(out,"endobj\n");
+		nByte += printf("%d 0 R\n", PG_BASE + i);
+	nByte += printf("]\n");
+	nByte += printf("/Count %d\n", nPage);
+	nByte += printf("/MediaBox [0 0 595 842]\n");
+	nByte += printf("/Resources << /Font << /F0 <<\n");
+	nByte += printf("/Type /Font\n");
+	nByte += printf("/Subtype /Type1\n");
+	nByte += printf("/BaseFont /Courier\n");
+	nByte += printf(">> >> >>\n");	/* end of resources dict */
+	nByte += printf(">>\n");
+	nByte += printf("endobj\n");
 }
 
-void genXref(FILE* out){
+void genXref(void){
 	xrefPos = nByte;
-	fprintf(out,"xref");
-	fprintf(out,"0 3");
-	fprintf(out,"0000000000 65535 f ");
-	fprintf(out,"%010d 00000 n \n", clPos);
-	fprintf(out,"%010d 00000 n \n", prPos);
-	fprintf(out,"%d %d\n", PG_BASE, nPage);
+	puts("xref");
+	puts("0 3");
+	puts("0000000000 65535 f ");
+	printf("%010d 00000 n \n", clPos);
+	printf("%010d 00000 n \n", prPos);
+	printf("%d %d\n", PG_BASE, nPage);
 	for (int i = 0; i < nPage; i++)
-		fprintf(out,"%010d 00000 n \n", pgPos[i]);
-	fprintf(out,"%d %d\n", PC_BASE, nPage);
+		printf("%010d 00000 n \n", pgPos[i]);
+	printf("%d %d\n", PC_BASE, nPage);
 	for (int i = 0; i < nPage; i++)
-		fprintf(out,"%010d 00000 n \n", pcPos[i]);
-	fprintf(out,"%d %d\n", SL_BASE, nPage);
+		printf("%010d 00000 n \n", pcPos[i]);
+	printf("%d %d\n", SL_BASE, nPage);
 	for (int i = 0; i < nPage; i++)
-		fprintf(out,"%010d 00000 n \n", slPos[i]);
+		printf("%010d 00000 n \n", slPos[i]);
 }
 
-void genTail(FILE* out){
-    fprintf(out,"trailer\n<< /Size %d /Root 1 0 R >>\n", SL_BASE + nPage);
-	fprintf(out,"startxref");
-	fprintf(out,"%d\n", xrefPos);
-	fprintf(out,"%%EOF");
+void genTail(void){
+    printf("trailer\n<< /Size %d /Root 1 0 R >>\n", SL_BASE + nPage);
+	puts("startxref");
+	printf("%d\n", xrefPos);
+	puts("%%EOF");
 }
 
 
 
-void genPdf(FILE* in,FILE* out){
+void genPdf(FILE* in){
 	int sl;
 	nByte = nPage = 0;
 	genHead();
-	genCl(out);
+	genCl();
 	do {
-		genPg(out);
-		sl = genPc(in,out);
-		genSl(sl,out);
+		genPg();
+		sl = genPc(in);
+		genSl(sl);
 		nPage++;
 	} while (nPage < NPAGE && !feof(in));
 	if (!feof(in)) {
 		fprintf(stderr, "text-to-pdf couldn't generate pdf : Too many pages.\n");
 		exit(-1);
 	}
-	genPr(out);
-	genXref(out);
-	genTail(out);
+	genPr();
+	genXref();
+	genTail();
 }
 
 
 int main(int argc, char* argv[]){
-	FILE *in;
+	FILE* in;
 	if (argc > 2) {
-		fprintf(stderr, "Here's how to use the program : ./bin/text-to-pdf [input file]\n");
+		fprintf(stderr, "Here's how to use the program : ./bin/text-to-pdf [filepath]\n");
 		exit(-1);
 	}
-	argv++;
-	if (*argv && !strcmp(*argv, "--help")) {
-		printf("Here's how to use the program : ./bin/text-to-pdf [input ]\n\n");
+	if (argv[1] && !strcmp(argv[1], "--help")) {
+		printf("Here's how to use the program : ./bin/text-to-pdf [filepath]\n\n");
 		exit(0);
 	}
-	if (*argv && !strcmp(*argv, "--version")) {
+	if (argv[1] && !strcmp(argv[1], "--version")) {
 		printf("%s %s\n%s\n", "text-to-pdf", "0.1", "Esteban795");
 		exit(0);
 	}
-	if (*argv) {
-		if (!(in=fopen(*argv, "r"))) {
+	if (argv[1]) {
+		if (!(in = fopen(argv[1], "r"))) {
 			perror("text-to-pdf");
 			exit(-1);
 		}
-	} else
+	} else {//no arguments means stdin is considered the input file
 		in = stdin;
-	int len = strlen(*argv);
-	char* outfile = malloc(sizeof(char) * len);
-	outfile[len - 4] = 'p';
-	outfile[len - 3] = 'd';
-	outfile[len - 2] = 'f';
-
-	printf("%s",outfile);
-	//FILE* out = fopen(outfile, "w");
-	//genPdf(in,out);
-	fclose(in);
-	//fclose(outfile);
-	free(outfile);
+	}
+	int len = strlen(argv[1]);
+	// char* outName = malloc(sizeof(char) * (len + 1));
+	// strcpy(outName,argv[1]);
+	// outName[len - 3] = 'p';
+	// outName[len - 2] = 'd';
+	// outName[len - 1] = 'f';
+	stdout = fopen("retest.txt","w");
+	genPdf(in);
+	//free(outName);
 	return 0;
 }
